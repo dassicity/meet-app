cfe6c0d27de6a1e2f0b70ec991630b99b4895f0a
Soumyanil Das <dassic@outlook.com>
2023-07-06 21:03:58 +0530

    added socket connection and remaining middlewares

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const { v4: uuidv4 } = require('uuid');
 const io = require('socket.io')(server);
 const { ExpressPeerServer } = require('peer');
 const url = require('url');
+const { Socket } = require('socket.io');
 const peerServer = ExpressPeerServer(server, {
     debug: true
 });
@@ -22,14 +23,49 @@ app.get("/", (req, res, next) => {
     res.sendFile(path.join(__dirname, "static", "index.html"));
 });
 
-//api to get the url
+//api to get the url - new url
 app.get("/join", (req, res, next) => {
     res.redirect(
         url.format({
-            pathname: `/join/${uuidv4}`
+            pathname: `/join/${uuidv4()}`,
+            query: req.query
         })
     )
 })
+
+//api to go to the url if uuid already exists
+app.get("/joinold/:meeting_id", (req, res, next) => {
+    res.redirect(
+        url.format({
+            pathname: req.params.meeting_id,
+            query: req.query
+        })
+    )
+});
+
+// api for joining a room
+app.get("/join/:rooms", (req, res, next) => {
+    res.render("room", {
+        roomid: req.params.rooms,
+        Myname: req.query.name
+    });
+});
+
+io.on("connection", (socket) => {
+
+    socket.on("join-room", (roomid, id, myname) => {
+        socket.join(room);
+        socket.to(roomid).broadcast.emit("user-connected", id, myname);
+
+        socket.on("tellname", (myname) => {
+            socket.to(roomid).broadcast.emit("addname", myname);
+        });
+
+        socket.on("disconnect", () => {
+            socket.to(roomid).broadcast.emit("user-disconnected", id);
+        });
+    });
+});
 
 const PORT = process.env.PORT;
 
